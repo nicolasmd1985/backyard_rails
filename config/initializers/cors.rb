f38306bfa73263dbd33ec1133cb8c95807eb621a
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2025-05-29 20:13:48 -0400

    change cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8081', 'http://localhost:8082', 'exp://192.168.1.38:8081', 'exp://192.168.1.38:8082'
+    origins 'http://localhost:8081', 'http://localhost:8082', 'exp://192.168.1.38:8081', 'exp://192.168.1.38:8082', 'https://nicolasmahecha.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
